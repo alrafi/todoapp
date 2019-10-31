d47bbe80a84c6fd49524e930ac8cec66ef31abd0
alrafi <hafisalrafi@gmail.com>
2019-11-01 00:37:11 +0700

    add about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <div className="ui container">
+        <h2>about page</h2>
+        <p>made by hafisalrafi, using React</p>
+      </div>
+    );
+  }
+}
+
+export default About;
