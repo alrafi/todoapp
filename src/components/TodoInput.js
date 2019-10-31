2e0a37873bfcfc214fa4b8fa9fee1633260b3774
alrafi <hafisalrafi@gmail.com>
2019-10-31 14:28:35 +0700

    add method onFormSubmit

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
 class TodoInput extends React.Component {
+  state = { content: '' };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.content);
+    this.setState({ content: '' });
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui big icon input">
-          <input type="text" />
-          <i class="inverted circular check link icon"></i>
+        <form className="ui big icon input" onSubmit={this.onFormSubmit}>
+          <input
+            type="text"
+            value={this.state.content}
+            onChange={e => this.setState({ content: e.target.value })}
+          />
+          <button
+            className="ui circular icon button"
+            style={{ marginLeft: 10 }}
+          >
+            <i className="inverted circular check link icon"></i>
+          </button>
         </form>
       </div>
     );
