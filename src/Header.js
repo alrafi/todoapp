d1d35fa2d13089639c0feb983367ae768215ea3a
alrafi <hafisalrafi@gmail.com>
2019-11-01 00:37:31 +0700

    add header for routing home and about page

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <div className="ui container">
+      <h1 class="ui header">To do App</h1>
+      <div className="ui secondary pointing menu">
+        <Link to="/" className="ui item">
+          Home
+        </Link>
+        <Link to="/about" className="ui item">
+          About
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
