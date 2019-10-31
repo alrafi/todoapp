a2f5d50bf1e8e94689540de73bfde1dfafad945d
alrafi <hafisalrafi@gmail.com>
2019-10-31 14:30:21 +0700

    make todoitem can get props from todolist component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 class TodoItem extends React.Component {
   render() {
-    return <div>Todo item</div>;
+    return (
+      <div className="ui relaxed items">
+        <div className="item">
+          <i className="sticky note outline icon big"></i>
+          <div className="middle aligned content">
+            <p className="header">{this.props.item.content}</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
