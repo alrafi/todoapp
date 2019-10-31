dd52ea236e9eff724bba77f1d26f3e4c16d70a51
alrafi <hafisalrafi@gmail.com>
2019-10-31 13:20:19 +0700

    add form input and styling

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class TodoInput extends React.Component {
   render() {
-    return <div>Todo Input</div>;
+    return (
+      <div className="ui segment">
+        <form className="ui big icon input">
+          <input type="text" />
+          <i class="inverted circular check link icon"></i>
+        </form>
+      </div>
+    );
   }
 }
 
